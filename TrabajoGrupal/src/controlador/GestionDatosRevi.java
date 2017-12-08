edb0119ac6b818076df36e394cfb5c44cd4b448d
ewin <Estudiante@lab0412>
2017-12-08 10:47:02 -0500

    datos guardados

--- TrabajoGrupal/src/controlador/GestionDatosRevi.java
+++ TrabajoGrupal/src/controlador/GestionDatosRevi.java
@@ -1,14 +1,12 @@
 package controlador;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import modelo.Articulo;
-import modelo.Atleta;
-import modelo.Empresa;
-import modelo.FichaInscripcion;
-import modelo.Medico;
-import modelo.Pais;
+
 import modelo.Revista;
 
 public class GestionDatosRevi {
@@ -16,5 +14,20 @@ private String pathPersona="Datos/Revistas.txt";
 	public GestionDatosRevi() {
 		
 	}
+public void guardarPersona(Revista r){
+	try{
+		
+		FileWriter file=new FileWriter(pathPersona,true);
+		BufferedWriter out=new BufferedWriter(file);
+		String registro=r.getNombre()+";"+r.getArticulos().getNumeroP();
+		
+		out.append(registro+"\n");
+		out.close();
+		file.close();
+		
+	}catch(IOException e){
+		e.printStackTrace();    
+	}
 	
+}
 }
